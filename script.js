6ee927dff305e88c5daa562da24f2c5ccc6eb811
Sterling <sterling32157@yahoo.com>
2023-06-09 18:17:24 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    let currentLetter = '';
+    let lastAttemptCorrect = false;
+    let acceptInput = true;
+    const letterDisplay = document.getElementById('letter-display');
+    const faceDisplay = document.getElementById('face-display');
+    const rainbowColors = ["orange", "yellow", "green", "blue", "purple", "red", "orange", "yellow", "green", "blue", "purple"];
+    let colorIndex = 0;
+    let colorInterval = null;
+
+    const generateLetter = () => {
+        currentLetter = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+        letterDisplay.textContent = currentLetter;
+        faceDisplay.textContent = '';
+        document.body.style.backgroundColor = 'white';
+        acceptInput = true;
+    };
+
+    const flashRainbow = () => {
+        acceptInput = false;
+        colorInterval = setInterval(() => {
+            document.body.style.backgroundColor = rainbowColors[colorIndex];
+            colorIndex = (colorIndex + 1) % rainbowColors.length;
+        }, 100);
+    };
+
+    const stopRainbow = () => {
+        clearInterval(colorInterval);
+        colorInterval = null;
+        colorIndex = 0;
+    };
+
+    const checkInput = (event) => {
+        if (!acceptInput) {
+            return;
+        }
+        if (event.key.toLowerCase() === currentLetter.toLowerCase()) {
+            lastAttemptCorrect = true;
+            faceDisplay.textContent = '😊';
+            flashRainbow();
+            setTimeout(() => {
+                stopRainbow();
+                generateLetter();
+            }, 2000);
+        } else {
+            lastAttemptCorrect = false;
+            faceDisplay.textContent = '☹️';
+            document.body.style.backgroundColor = 'red';
+            acceptInput = false;
+            setTimeout(generateLetter, 2000);
+        }
+    };
+
+    document.addEventListener('keypress', checkInput);
+    generateLetter();
+});
